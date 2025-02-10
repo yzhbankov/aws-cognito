9e5e563dc7b4d0c56845b47ebdeadcfd1a74fd34
yzhbankov <yzhbankov@redpointpositioning.com>
2025-02-10 02:58:52 +0000

    wip

--- apps/client/src/app/Example.tsx
+++ apps/client/src/app/Example.tsx
@@ -9,7 +9,7 @@ export function Example() {
         (async () => {
             try {
                 const token = auth.user?.access_token;
-                const response = await fetch('https://api.example.com/posts', {
+                const response = await fetch('https://3yb7wmqypk.execute-api.us-east-1.amazonaws.com/prod/api/v1/example', {
                     headers: {
                         Authorization: `Bearer ${token}`,
                     },
