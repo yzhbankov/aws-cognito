9edbfad36aeb741204d2dacbcf428177b0331b30
yzhbankov <yzhbankov@redpointpositioning.com>
2025-02-17 01:04:48 +0000

    Make single App component

--- apps/client/src/app/AppContainer.tsx
+++ apps/client/src/app/AppContainer.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import {App} from './App';
+import React, {useEffect} from 'react';
+import {useAuth} from 'react-oidc-context';
+import {Example} from './Example';
 
 export function AppContainer() {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<App/>}/>
-            </Routes>
-        </>
-    );
+    const auth = useAuth();
+
+    useEffect(() => {
+        // Clear the query parameters if the user is authenticated
+        if (auth.isAuthenticated) {
+            const url = new URL(window.location.href);
+            url.search = ''; // Clear the query parameters
+            window.history.replaceState({}, document.title, url.toString());
+        }
+    }, [auth.isAuthenticated]);
+
+    switch (auth.activeNavigator) {
+        case "signinSilent":
+            return <div>Signing you in...</div>;
+        case "signoutRedirect":
+            return <div>Signing you out...</div>;
+    }
+
+    if (auth.isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (auth.error) {
+        return <div>Oops... {auth.error.message}</div>;
+    }
+
+    if (auth.isAuthenticated) {
+        return (
+            <div>
+                Hello {auth.user?.profile.sub}{" "}
+                <Example />
+                <button onClick={() => auth.removeUser()}>Log out</button>
+            </div>
+        );
+    }
+
+    return <button onClick={() => auth.signinRedirect()}>Log in</button>;
 }
-
-AppContainer.defaultProps = {};
-
-AppContainer.propTypes = {};
